abecbba92965d6a0655a699e82084725e3d416e5
amandafralve <amanda.fralve@gmail.com>
2026-04-13 19:21:52 -0300

    add: revisao perceptron

---
+++ revisao/revisao_perceptron/notebook/perceptron.py
@@ -0,0 +1,18 @@
+class PerceptronSimples:
+    def __init__(self, peso_renda=0.3, peso_historico=0.8, bias = 7.0):
+        # Perceptron Simples
+        self.peso_renda = peso_renda
+        self.peso_historico = peso_historico
+        self.bias = bias
+
+    def prever(self, renda, historico):
+        # Equação de decisão
+        z = (renda * self.peso_renda) + (historico * self.peso_historico) + self.bias
+
+        # Função de ativação
+        if z >=0:
+            return "Aprovado"
+        else:
+            return "Reprovado"
+
+        
